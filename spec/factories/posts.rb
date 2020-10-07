4238cdbe396f5e26057c0215b1cc08d41661047c
ryotasykt <ryotasykt@gmail.com>
2020-10-08 01:34:42 +0900

    postモデルのテスト

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,5 +1,15 @@
 FactoryBot.define do
   factory :post do
-    
+    name { Faker::Name.name }
+    description { Faker::Lorem.paragraph }
+    member_id { Faker::Number.between(from: 2, to: 8) }
+    transportation_id { Faker::Number.between(from: 2, to: 10) }
+    satisfaction_level_id { Faker::Number.between(from: 2, to: 6) }
+    prefecture_id { Faker::Number.between(from: 2, to: 48) }
+    association :user
+  
+    after(:build) do |sample|
+      sample.image.attach(io: File.open('app/assets/images/camera.png'), filename: 'camera.png')
+    end
   end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,58 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @post = FactoryBot.build(:post)
+  end
+
+  describe '新規投稿' do
+    context '新規投稿がうまくいくとき' do
+      it 'image、name、description、member、transportation、satisfaction_level、prefectureが存在すれば出品できる' do
+        expect(@post).to be_valid
+      end
+    end
+
+    context '新規投稿がうまくいかないとき' do
+      it 'imageが空だと投稿できない' do
+        @post.image = nil
+        @post.valid?
+        expect(@post.errors.full_messages).to include("Image can't be blank")
+      end
+      it 'nameが空だと投稿できない' do
+        @post.name = ''
+        @post.valid?
+        expect(@post.errors.full_messages).to include("Name can't be blank")
+      end
+      it 'descriptionが空だと出品できない' do
+        @post.description = ''
+        @post.valid?
+        expect(@post.errors.full_messages).to include("Description can't be blank")
+      end
+      it 'memberが選択されていないと出品できない' do
+        @post.member_id = 1
+        @post.valid?
+        expect(@post.errors.full_messages).to include('Member Select')
+      end
+      it 'transportationが選択されていないと出品できない' do
+        @post.transportation_id = 1
+        @post.valid?
+        expect(@post.errors.full_messages).to include('Transportation Select')
+      end
+      it 'satisfaction_levelが選択されていないと出品できない' do
+        @post.satisfaction_level_id = 1
+        @post.valid?
+        expect(@post.errors.full_messages).to include('Satisfaction level Select')
+      end
+      it 'prefectureが選択されていないと出品できない' do
+        @post.prefecture_id = 1
+        @post.valid?
+        expect(@post.errors.full_messages).to include('Prefecture Select')
+      end
+      it 'ユーザーが紐付いていないと出品できない' do
+        @post.user = nil
+        @post.valid?
+        expect(@post.errors.full_messages).to include('User must exist')
+      end
+    end
+  end
 end
