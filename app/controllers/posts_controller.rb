cc176aa7dff79a18c7562f39c648204bf9f9f7df
ryotasykt <ryotasykt@gmail.com>
2020-10-07 15:14:40 +0900

    Active Storageの導入

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,4 +3,11 @@ class PostsController < ApplicationController
 
   def index
   end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:name, :image, :description, :member_id, :transportation_id, :satisfaction_level_id, :prefecture_id).merge(user_id: current_user.id)
+  end
+
 end
